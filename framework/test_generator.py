a3cf8cb45b6544036cc3bf8666546dab78bba6f3
alenkruth <alenkruth@gmail.com>
2021-06-23 11:57:50 +0530

    fix directory ordering

--- framework/test_generator.py
+++ framework/test_generator.py
@@ -14,7 +14,7 @@ def yapsy_test(test_file_dir="bpu/"):
     # Loop around and find the plugins and writes the contents from the plugins into an asm file
     for plugin in manager.getAllPlugins():
         name = (str(plugin.plugin_object).split(".",1))
-        f = open('tests/tests/'+((name[1].split(" ",1))[0])+'.S',"w")
+        f = open('bpu/tests/'+((name[1].split(" ",1))[0])+'.S',"w")
         asm = asm_header + plugin.plugin_object.generate_asm() + asm_footer
         f.write(asm)
         f.close()
