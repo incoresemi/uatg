3ad4d49f82ea6e517155efee631c7933e7be58ea
purushothaman-p <purushothsankari@yahoo.com>
2021-06-25 15:45:57 +0530

    log parser created

---
+++ framework/log_parser.py
@@ -0,0 +1,26 @@
+import re
+from regex_formats import *
+
+
+def main():
+    f = open('../asm_tests/build/log', "r")
+    log_file = f.read()
+
+    bpu_match_result = re.findall(bpu_match_pattern, log_file)
+    btb_hit_result = re.findall(btb_hit_pattern, log_file)
+    pred_req_result = re.findall(pred_req_pattern, log_file)
+    train_data_result = re.findall(train_data_pattern, log_file)
+    train_existing_result = re.findall(train_existing_pattern, log_file)
+    alloc_newind_result = re.findall(alloc_newind_pattern, log_file)
+    conflict_result = re.findall(conflict_pattern, log_file)
+    pushing_to_ras_result = re.findall(pushing_to_ras_pattern, log_file)
+    choosing_top_ras_result = re.findall(choosing_top_ras_pattern, log_file)
+    new_ghr_result = re.findall(new_ghr_pattern, log_file)
+    bht_ind_target_result = re.findall(bht_ind_target_pattern, log_file)
+    misprediction_result = re.findall(misprediction_pattern, log_file)
+
+    print(bpu_match_result)
+
+
+if __name__ == '__main__':
+    main()
