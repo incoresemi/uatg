60668a95b267f56a7060e1cd44adf6500c774302
purushothaman-p <purushothsankari@yahoo.com>
2021-06-25 17:59:52 +0530

    added simple test condition to gshare_fa_ghr_zeros_01

--- framework/bpu/gshare_fa_ghr_zeros_01.py
+++ framework/bpu/gshare_fa_ghr_zeros_01.py
@@ -7,16 +7,18 @@ class gshare_fa_ghr_zeros_01(IPlugin):
     def __init__(self):
         self.ghr_width = 8
 
-    def generate_asm(self):
+    def generate_asm(self, bpu_class):
         '''
           the for loop iterates ghr_width + 2 times printing an 
           assembly program which contains ghr_width + 2 branches which 
           will are *NOT TAKEN*. This fills the ghr with zeros
         '''
-        ghr_width = self.ghr_width
-        loop_count = ghr_width + 2
+        if bpu_class.history_len == 0:
+            return None
 
-        asm = "  addi t0,x0,1\n"
+        loop_count = bpu_class.history_len + 2
+        asm = "## test: gshare_fa_ghr_zeros_01\n\n\n\n"
+        asm += "  addi t0,x0,1\n"
 
         for i in range(1, loop_count):
             asm = asm + "branch_" + str(i) + ":\n"
