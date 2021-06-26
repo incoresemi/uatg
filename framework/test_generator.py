cf933727d406dd4d0e37a2d922b8fd43cb4643cf
purushothaman-p <purushothsankari@yahoo.com>
2021-06-26 10:58:23 +0530

    minor optimizations

--- framework/test_generator.py
+++ framework/test_generator.py
@@ -21,7 +21,7 @@ def yapsy_test(module_class, test_file_dir="bpu/"):
         os.rmdir(test_file_dir + "tests/")
     except OSError:
         pass
-    os.makedirs(test_file_dir + "tests/", exist_ok=False)
+    os.mkdirs(test_file_dir + "tests/")
     # Loop around and find the plugins and writes the contents from the plugins into an asm file
     for plugin in manager.getAllPlugins():
         name = (str(plugin.plugin_object).split(".", 1))
