295c7e1bd1b230aae039ef386062f1a9b3d434bb
alenkruth <alenkruth@gmail.com>
2021-06-28 11:42:40 +0530

    internally check for parameters and return test if suitable parameters are set

--- framework/bpu/gshare_fa_ghr_zeros_01.py
+++ framework/bpu/gshare_fa_ghr_zeros_01.py
@@ -7,22 +7,31 @@ class gshare_fa_ghr_zeros_01(IPlugin):
     def __init__(self):
         self.ghr_width = 8
 
-    def generate_asm(self, bpu_class):
+    def generate_asm(self, _instance):
         '''
           the for loop iterates ghr_width + 2 times printing an 
           assembly program which contains ghr_width + 2 branches which 
           will are *NOT TAKEN*. This fills the ghr with zeros
         '''
-        if bpu_class.history_len == 0:
-            return None
+        _history_len = _instance['history_len']
+        _en_bpu = _instance['instantiate']
 
-        loop_count = bpu_class.history_len + 2
-        asm = "\n\n## test: gshare_fa_ghr_zeros_01 ##\n\n"
-        asm += "  addi t0,x0,1\n"
+        if (_en_bpu and _history_len):
+            loop_count = _history_len + 2
+            asm = "\n\n## test: gshare_fa_ghr_zeros_01 ##\n\n"
+            asm += "  addi t0,x0,1\n"
 
-        for i in range(1, loop_count):
-            asm = asm + "branch_" + str(i) + ":\n"
-            asm = asm + "  beq t0,x0,branch_" + str(i) + "\n"
-            asm = asm + "  addi t0,t0,1\n"
+            for i in range(1, loop_count):
+                asm = asm + "branch_" + str(i) + ":\n"
+                asm = asm + "  beq t0,x0,branch_" + str(i) + "\n"
+                asm = asm + "  addi t0,t0,1\n"
 
-        return (asm)
+            return (asm)
+
+        else:
+            return (0)
+
+    def check_log(self):
+        """
+          check if all the ghr values are zero throughout the test
+        """
--- framework/bpu/gshare_fa_ras_push_pop_01.py
+++ framework/bpu/gshare_fa_ras_push_pop_01.py
@@ -1,6 +1,8 @@
 #python script to automate test 11 in microarch test
 
 from yapsy.IPlugin import IPlugin
+#import ruamel
+#from ruamel.yaml import YAML
 
 
 class gshare_fa_ras_push_pop_01(IPlugin):
@@ -8,23 +10,35 @@ class gshare_fa_ras_push_pop_01(IPlugin):
     def __init__(self):
         self.recurse_level = 5
 
-    def generate_asm(self, bpu_class):
+    def generate_asm(self, _bpu_dict):
         """
         reg x30 is used as looping variable. reg x31 used as a temp variable
         """
-        recurse_level = self.recurse_level
-        no_ops = '\taddi x31, x0, 5\n\taddi x31, x0, -5\n'
-        asm = '\taddi x30, x0, ' + str(recurse_level) + '\n'
-        asm += '\tcall x1, lab1\n\tbeq  x30, x0, end\n'
+        _en_ras = _bpu_dict['ras_depth']
+        _en_bpu = _bpu_dict['instantiate']
 
-        for i in range(1, recurse_level + 1):
-            asm += 'lab' + str(i) + ':\n'
-            if (i == recurse_level):
-                asm += '\taddi x30, x30, -1\n'
-            else:
-                asm += no_ops * 3 + '\tcall x' + str(i + 1) + ', lab' + str(
-                    i + 1) + '\n'
-            asm += no_ops * 3 + '\tret\n'
-        asm += 'end:\n\tnop\n'
+        if (_en_ras and _en_bpu):
+            recurse_level = self.recurse_level
+            no_ops = '\taddi x31, x0, 5\n\taddi x31, x0, -5\n'
+            asm = '\taddi x30, x0, ' + str(recurse_level) + '\n'
+            asm += '\tcall x1, lab1\n\tbeq  x30, x0, end\n'
 
-        return (asm)
+            for i in range(1, recurse_level + 1):
+                asm += 'lab' + str(i) + ':\n'
+                if (i == recurse_level):
+                    asm += '\taddi x30, x30, -1\n'
+                else:
+                    asm += no_ops * 3 + '\tcall x' + str(i + 1) + ', lab' + str(
+                        i + 1) + '\n'
+                asm += no_ops * 3 + '\tret\n'
+            asm += 'end:\n\tnop\n'
+            return (asm)
+
+        else:
+            return (0)
+
+    def check_log(self):
+        """
+          check for pushes and pops in this file. 
+          There should be 8 pushes and 4 pops (unrelated - should check why that happens, there should be 8 pops)
+        """
