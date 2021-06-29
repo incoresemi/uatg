32bb9919c2a87afd9b06d1a57f3094a0082747b0
purushothaman-p <purushothsankari@yahoo.com>
2021-06-29 11:36:46 +0530

    check_log for fence

--- framework/bpu/gshare_fa_fence_01.py
+++ framework/bpu/gshare_fa_fence_01.py
@@ -1,6 +1,8 @@
-## This program fences the CPU and checks if the BTB entries are invalidated.
+# This program fences the CPU and checks if the BTB entries are invalidated.
 
 from yapsy.IPlugin import IPlugin
+import regex_formats as rf
+import re
 
 
 class gshare_fa_fence_01(IPlugin):
@@ -10,6 +12,7 @@ class gshare_fa_fence_01(IPlugin):
     """
 
     def __init__(self):
+        super().__init__()
         self.recurse_level = 5
 
     def generate_asm(self, _bpu_dict):
@@ -20,26 +23,26 @@ class gshare_fa_fence_01(IPlugin):
         """
         _en_ras = _bpu_dict['ras_depth']
         _en_bpu = _bpu_dict['instantiate']
-
+        #TODO why  recursing in the asm test?
         if _en_ras and _en_bpu:
             recurse_level = self.recurse_level
-            no_ops = "  addi x31,x0,5\n  addi x31,x0,-5\n"
-            asm = "  addi x30,x0," + str(recurse_level) + "\n"
-            asm = asm + "  call x1,lab1\n  beq  x30,x0,end\n  fence.i\n"
+            no_ops = "\taddi x31,x0,5\n  addi x31,x0,-5\n"
+            asm = "\taddi x30,x0," + str(recurse_level) + "\n"
+            asm = asm + "\tcall x1,lab1\n\tbeq x30,x0,end\n\tfence.i\n"
 
             for i in range(1, recurse_level + 1):
-                asm = asm + "lab" + str(i) + ":\n"
+                asm += "lab" + str(i) + ":\n"
                 if i == recurse_level:
-                    asm = asm + "  fence.i\n  addi x30,x30,-1\n"
+                    asm += "\tfence.i\n\taddi x30,x30,-1\n"
                 else:
                     asm = asm + no_ops * 3 + "  call x" + str(
                         i + 1) + ", lab" + str(i + 1) + "\n"
-                asm = asm + no_ops * 3 + "  ret\n"
-            asm = asm + "end:\n  nop\n"
+                asm = asm + no_ops * 3 + "\tret\n"
+            asm = asm + "end:\n\tnop\n"
 
             return asm
         else:
-            return (0)
+            return 0
 
     def check_log(self, log_file_path):
         """
@@ -51,3 +54,9 @@ class gshare_fa_fence_01(IPlugin):
         f = open(log_file_path, "r")
         log_file = f.read()
         f.close()
+
+        fence_executed_result = re.findall(rf.fence_executed_pattern, log_file)
+        if len(fence_executed_result) <= 1:
+            # check for execution of more than one fence inst
+            return False
+        return True
