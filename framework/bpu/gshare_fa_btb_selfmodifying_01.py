9a5d078c3e2b30be6caac880e3d6a78af20611b9
purushothaman-p <purushothsankari@yahoo.com>
2021-06-29 12:00:12 +0530

    check_log for self_modifying

--- framework/bpu/gshare_fa_btb_selfmodifying_01.py
+++ framework/bpu/gshare_fa_btb_selfmodifying_01.py
@@ -1,6 +1,8 @@
 ## Python program to generate the selfmodifyinf assembly test program
 
 from yapsy.IPlugin import IPlugin
+import regex_formats as rf
+import re
 
 
 class gshare_fa_btb_selfmodifying_01(IPlugin):
@@ -14,20 +16,20 @@ class gshare_fa_btb_selfmodifying_01(IPlugin):
         """
         _en_bpu = _bpu_dict['instantiate']
 
-        if (_en_bpu):
+        if _en_bpu:
             asm = ".option norvc\n\n"
-            asm = asm + "  addi t3,x0,0\n\taddi t4,x0,3\n\tjal x0,first\n\n"
-            asm = asm + "first:\n\taddi t3,t3,1\n\tbeq t3,t4,end\n\tjal x0,first" \
-                  + "\n\tjal x0,fin\n\n"
-            asm = asm + "end:\n\taddi x0,x0,0\n\taddi t0,x0,1\n\tslli t0,t0,31" \
-                  + "\n\taddi t0,t0,0x3a4\n"
-            asm = asm + "\taddi t1,x0,0x33\n\taddi t3,x0,4\n\tsw t1,0(t0)\n\t" \
-                  + "fence.i\n\tjal x0,first\n\n"
+            asm += "  addi t3,x0,0\n\taddi t4,x0,3\n\tjal x0,first\n\n"
+            asm += "first:\n\taddi t3,t3,1\n\tbeq t3,t4,end\n\tjal x0,first" \
+                   + "\n\tjal x0,fin\n\n"
+            asm += "end:\n\taddi x0,x0,0\n\taddi t0,x0,1\n\tslli t0,t0,31" \
+                   + "\n\taddi t0,t0,0x3a4\n"
+            asm += "\taddi t1,x0,0x33\n\taddi t3,x0,4\n\tsw t1,0(t0)\n\t" \
+                   + "fence.i\n\tjal x0,first\n\n"
             asm = asm + "fin:\n"
 
             return asm
         else:
-            return (0)
+            return 0
 
     def check_log(self, log_file_path):
         """
@@ -38,3 +40,10 @@ class gshare_fa_btb_selfmodifying_01(IPlugin):
         f = open(log_file_path, "r")
         log_file = f.read()
         f.close()
+
+        fence_executed_result = re.findall(rf.fence_executed_pattern, log_file)
+        if len(fence_executed_result) <= 1:
+            # check for execution of more than one fence inst
+            return False
+        return True
+
