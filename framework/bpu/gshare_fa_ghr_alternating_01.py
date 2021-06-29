01a0ea8bcda125268171dcc395d16a0ff81df60e
alenkruth <alenkruth@gmail.com>
2021-06-29 17:06:37 +0530

    modified code to fill ghr with alternating zeros and ones

--- framework/bpu/gshare_fa_ghr_alternating_01.py
+++ framework/bpu/gshare_fa_ghr_alternating_01.py
@@ -7,9 +7,12 @@ class gshare_fa_ghr_alternating_01(IPlugin):
 
     def __init__(self):
         super().__init__()
-        self.btb_depth = 32
-        self._history_len = 8
-        self.overflow_times = 0
+        ##self.btb_depth = 32
+        ##self._history_len = 8
+        ##self.overflow_times = 0
+        #self.btb_depth = 32
+        #self.history_len = 8
+        pass
 
     def generate_asm(self, _bpu_dict):
         """
@@ -17,32 +20,38 @@ class gshare_fa_ghr_alternating_01(IPlugin):
         register with alternating 0's and 1's pattern. eg. 010101010....
         history_len = the size of the Global History Register (ghr) in bits.
                       By default history_len is set to be 8 bits.
-        overflow_times = number of times to repeatedly apply the pattern after
-                         filling the Register. By default overflow_times is set
-                         to 0
-
         The generated assembly code will use the t0 register to alternatively
         enter and exit branches.
         """
 
         _en_bpu = _bpu_dict['instantiate']
-        self._history_len = _bpu_dict['history_len']
-        overflow_times = self.overflow_times
+        _history_len = _bpu_dict['history_len']
 
-        if _en_bpu and self._history_len:
-            asm = '\taddi t0, x0, 1\t\n'
-            asm = asm + '\tbeq  t0, x0, lab0\t\n\taddi t0, t0, -1\t\n'
+        if (_en_bpu and _history_len):
+            asm = ".option norvc\n"
+            asm = asm + '\taddi t0,x0,1\n'
+            asm = asm + '\taddi t1,x0,1\n\taddi t2,x0,2\n\n'
+            asm = asm + '\tbeq  t0,x0,lab0\n'
 
-            for i in range(self.history_len + overflow_times):
+            if (_history_len % 2):
+                _history_len = _history_len + 1
+            ## the assembly program is structured in a way that
+            ## there are odd number of labels.
+
+            for i in range(_history_len):
                 if i % 2:
-                    asm += 'lab' + str(i) + ':\n'
-                    asm += '\taddi t0, t0, 1\t\n'
-                    asm += '\tbeq  t0, x0, lab' + str(i + 1) + '\t\n'
-                    asm += '\taddi t0, t0, -1\t\n'
+                    asm = asm + 'lab' + str(i) + ':\n'
+                    asm = asm + '\taddi t0,t0,1\n'
+                    asm = asm + '\tbeq  t0,x0,lab' + str(i + 1) + '\n'
                 else:
-                    asm += 'lab' + str(i) + ':\n'
-                    asm += '\tbeq  t0, x0, lab' + str(i + 1) + '\t\n'
-            asm += 'lab' + str(self.history_len + overflow_times) + ':\n'
+                    asm = asm + 'lab' + str(i) + ':\n'
+                    asm = asm + '\taddi t0,t0,-1\n'
+                    asm = asm + '\tbeq  t0,x0,lab' + str(i + 1) + '\t\n'
+            asm = asm + 'lab' + str(_history_len) + ':\n'
+            asm = asm + '\taddi t0,t0,-1\n\n'
+            asm = asm + '\taddi t1,t1,-1\n\taddi t2,t2,-1\n'
+            asm = asm + '\tbeq  t1,x0,lab0\n\taddi t0,t0,2\n'
+            asm = asm + '\tbeq  t2,x0,lab0\n'
             return asm
         else:
             return 0
