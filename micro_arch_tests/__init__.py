d48c432460e6dbd7a9a7d8f8a1e5501f13fd745f
purushothaman-p <purushothsankari@yahoo.com>
2021-07-06 12:44:44 +0530

    updated __init__.py

---
+++ micro_arch_tests/__init__.py
@@ -0,0 +1,7 @@
+# See LICENSE.incore for details
+
+"""Top-level package for RISC-V Micro Architecture Test Generator."""
+
+__author__ = """InCore Semiconductors Pvt Ltd"""
+__email__ = 'incorebot@gmail.com'
+__version__ = 'dev-0.0.1'
