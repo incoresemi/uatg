050c5e4d0225fbe98bfe309c4e9484c9c61ac316
alenkruth <alenkruth@gmail.com>
2021-08-24 15:44:04 +0530

    rename package to utg

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*parts):
 
 
 def read_requires():
-    with open(os.path.join(here, "uarch_test/requirements.txt"),
+    with open(os.path.join(here, "utg/requirements.txt"),
               "r") as reqfile:
         return reqfile.read().splitlines()
 
@@ -28,7 +28,7 @@ setup_requirements = []
 test_requirements = []
 
 setup(
-    name='uarch_test',
+    name='utg',
     version='dev-0.0.1',
     description="RISC-V micro-architectural tests generator",
     long_description=readme + '\n\n',
@@ -42,15 +42,15 @@ setup(
     author_email='incorebot@gmail.com',
     license="BSD-3-Clause",
     packages=find_packages(),
-    package_dir={'uarch_test': 'uarch_test'},
-    package_data={'uarch_test': ['requirements.txt', 'target/*', 'env/*']},
+    package_dir={'utg': 'utg'},
+    package_data={'utg': ['requirements.txt', 'target/*', 'env/*']},
     install_requires=read_requires(),
     python_requires='>=3.6.0',
     entry_points={
-        'console_scripts': ['uarch_test=uarch_test.main:cli'],
+        'console_scripts': ['utg=utg.main:cli'],
     },
     include_package_data=True,
-    keywords='uarch_test',
+    keywords='utg',
     test_suite='tests',
     tests_require=test_requirements,
     zip_safe=False,
