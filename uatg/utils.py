ae99c700ba90b0676069d5d88403f91001b7cbd6
alenkruth <alenkruth@gmail.com>
2021-09-22 12:53:01 +0530

    update generation of sample paths in config.ini and generation of rv64_isa.yaml

--- uatg/utils.py
+++ uatg/utils.py
@@ -434,10 +434,14 @@ def create_config_file(config_path):
           'same as work_dir\n' \
           'linker_dir = /home/user/myquickstart/work/' \
           '\n\n# Path to the yaml files containing DUT Configuration.\n' \
-          'configuration_files = /home/user/myquickstart/dut_config.yaml' \
+          'configuration_files = /home/user/myquickstart/isa_config.yaml,'\
+          '/home/user/myquickstart/core_config.yaml,' \
+          '/home/user/myquickstart/custom_config.yaml,' \
+          '/home/user/myquickstart/csr_grouping.yaml' \
           '\n\n# Absolute Path of the yaml file contain' \
           'ing the signal aliases of the DUT ' \
-          '\nalias_file = /home/user/myquickstart/aliasing.yaml' \
+          '\nalias_file = /home/user/myquickstart/chromite_uatg_tests/'\
+          'aliasing.yaml' \
           '\n\n# [True, False] If the gen_test_' \
           'list flag is True, the test_list.yaml needed for running tests in ' \
           'river_core are generated automatically.\n# Unless you want to ' \
@@ -490,13 +494,14 @@ def create_dut_config_files(dut_config_path):
     s6 = s2 * 3
     s8 = s4 * 2
     rv64i_isa = f'hart_ids: [0]\nhart0:\n{s2}custom_exceptions:\n{s4}- cause' \
-                f'_val: 25\n{s4}cause_name: halt_ebreak\n{s4}priv_mode: M' \
-                f'\n{s4}- cause_val: 26\n{s4}cause_name: halt_trigger\n{s4}' \
-                f'priv_mode: M\n{s4}- cause_val: 28\n{s4}cause_name: halt_' \
-                f'step\n{s4}priv_mode: M\n{s4}- cause_val: 29\n{s4}cause_name' \
-                f': halt_reset\n{s4}priv_mode: M\n{s2}custom_interrupts:' \
-                f'\n{s4}- cause_val: 16\n{s4}cause_name: debug_interrupt' \
-                f'\n{s4}on_reset_enable: 1\n{s4}priv_mode : M\n{s2}ISA: ' \
+                f'_val: 25\n{s4}  cause_name: halt_ebreak\n{s4}  priv_mode: M' \
+                f'\n{s4}- cause_val: 26\n{s4}  cause_name: halt_trigger\n{s4}' \
+                f'  priv_mode: M\n{s4}- cause_val: 28\n{s4}  cause_name: halt_'\
+                f'step\n{s4}  priv_mode: M\n{s4}- cause_val: 29\n' \
+                f'{s4}  cause_name: halt_reset\n'\
+                f'{s4}  priv_mode: M\n{s2}custom_interrupts:' \
+                f'\n{s4}- cause_val: 16\n{s4}  cause_name: debug_interrupt' \
+                f'\n{s4}  on_reset_enable: 1\n{s4}  priv_mode : M\n{s2}ISA: ' \
                 f'RV64IMACSUZicsr_Zifencei\n{s2}User_Spec_Version: "2.3"' \
                 f'\n{s2}pmp_granularity: 1\n{s2}physical_addr_sz: 32\n{s2}' \
                 f'supported_xlen:\n{s4}- 64\n'
