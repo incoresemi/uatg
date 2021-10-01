45e51ce8488f3439bd2eafba10fead9d477aa39c
alenkruth <alenkruth@gmail.com>
2021-10-01 16:01:46 +0530

    fix logic for uatg_decoder_test_4

--- uatg/test_generator.py
+++ uatg/test_generator.py
@@ -111,7 +111,7 @@ def generate_tests(work_dir, linker_dir, modules, config_dict, test_list,
                 asm = license_str + includes + test_entry
                 # Appending Coding Macros & Instructions
                 asm += rvcode_begin + asm_body
-                if test_name == 'uatg_decoder_arithmetic_insts_4':
+                if test_name != 'uatg_decoder_arithmetic_insts_4':
                     asm += rvcode_end
                     # Appending RVTEST_DATA macros and data values
                     asm += rvtest_data_begin + rvtest_data(bit_width=32,
