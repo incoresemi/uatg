c271e330ca4a3175882b3b6e6e024ea1e4eaff0e
alenkruth <alenkruth@gmail.com>
2021-10-12 12:36:55 +0530

    changes to parse and filter ISA string

--- uatg/test_generator.py
+++ uatg/test_generator.py
@@ -131,7 +131,8 @@ def generate_tests(work_dir, linker_dir, modules, config_dict, test_list,
         if test_list:
             logger.info(f'Creating test_list for the {module}')
             test_list_dict.update(
-                generate_test_list(work_tests_dir, uarch_dir, test_list_dict))
+                generate_test_list(work_tests_dir, uarch_dir, isa.lower(),
+                                   test_list_dict))
 
     logger.info('****** Finished Generating Tests ******')
 
