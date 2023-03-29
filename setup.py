9f1e2dc314540186d8bc326772755cea2067c8cf
karans5 <karan.s@incoresemi.com>
2023-03-29 15:00:05 +0530

    Revert "Bump version: 1.6.0 → 1.6.1"
    This reverts commit ad6b2f992aaf68eb74ad781b49ddb10fa7cb20e6.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = []
 
 setup(
     name='uatg',
-    version='1.6.1',
+    version='1.6.0',
     description="UATG - Micro-Architecture (µArch) Tests Generator",
     long_description=readme + '\n\n',
     classifiers=[
