2416fbcbef4fce03ee99c9aaf814d208cbbb3e28
Edwin Joy <43539365+edwin7026@users.noreply.github.com>
2023-11-10 10:50:26 +0000

    bumped 1.12.0 ----> 1.12.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = []
 
 setup(
     name='uatg',
-    version='1.12.0',
+    version='1.12.1',
     description="UATG - Micro-Architecture (µArch) Tests Generator",
     long_description=readme + '\n\n',
     classifiers=[
